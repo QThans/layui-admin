00e859c66131e313741352caf4f611a801cb3490
Thans <360641274@qq.com>
2019-05-10 20:35:39 +0800

    跳转页面居中错误修复。 命名错误

--- src/controller/Index.php
+++ src/controller/Index.php
@@ -27,11 +27,11 @@ class Index
 
     public function dashboard()
     {
-        $dashbord = new Dashbord();
+        $dashboard = new Dashbord();
         $mysql = Db::query("select VERSION() as version");
         $mysql = $mysql[0]['version'];
         $mysql = empty($mysql) ? lang('UNKNOWN') : $mysql;
-        $dashbord->card()->title('系统信息')->datas([
+        $dashboard->card()->title('系统信息')->datas([
             'Thinkphp版本' => App::version(),
             '服务器版本' => $_SERVER['SERVER_SOFTWARE'],
             '系统' => PHP_OS,
@@ -39,11 +39,11 @@ class Index
             'MySql版本' => $mysql,
             '剩余空间' => round((@disk_free_space(".") / (1024 * 1024)), 2) . 'M',
         ]);
-        $dashbord->card()->title('联系信息')->datas([
+        $dashboard->card()->title('联系信息')->datas([
             '官网' => '<a href=\'https://layuiadmin.com\' target="_blank">LayuiAdmin-TP</a></a>',
             '商务联系' => '<img src="http://cdn.inge.vip/thans.jpeg">',
             '为我点赞' => '<a target="_blank" href=\'https://gitee.com/thans/layuiAdmin/stargazers\'><img src=\'https://gitee.com/thans/layuiAdmin/badge/star.svg?theme=white\' alt=\'star\'></img></a>',
         ]);
-        return $dashbord->render();
+        return $dashboard->render();
     }
 }

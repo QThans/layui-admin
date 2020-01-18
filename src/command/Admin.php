8411a4718b27c53a1c1a86368620402fef542180
thans <thans@thans.cn>
2020-01-19 00:28:28 +0800

    优化配置文件生成

--- src/command/Admin.php
+++ src/command/Admin.php
@@ -30,32 +30,38 @@ class Admin extends \think\console\Command
 
     public function createConfig($output)
     {
-        $path = app()->getAppPath().'..'.DIRECTORY_SEPARATOR.'.env';
-        if (file_exists($path)
+        $path = app()->getAppPath() . '..' . DIRECTORY_SEPARATOR . '.env';
+        if (
+            file_exists($path)
             && strpos(file_get_contents($path), '[FILESYSTEM]')
         ) {
             $output->writeln('filesystem is configured');
         } else {
-            file_put_contents($path,
-                PHP_EOL."[FILESYSTEM]".PHP_EOL."DRIVER=public".PHP_EOL,
-                FILE_APPEND);
+            copy(app()->getAppPath() . '..' . DIRECTORY_SEPARATOR . '.example.env', $path);
+            file_put_contents(
+                $path,
+                PHP_EOL . "[FILESYSTEM]" . PHP_EOL . "DRIVER=public" . PHP_EOL,
+                FILE_APPEND
+            );
             $output->writeln('filesystem has configured');
         }
     }
 
     public function createMigrations($output)
     {
-        $migrationsPath = app()->getAppPath().'..'.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations';
-        copy_dir(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations',
-            $migrationsPath);
+        $migrationsPath = app()->getAppPath() . '..' . DIRECTORY_SEPARATOR . 'database' . DIRECTORY_SEPARATOR . 'migrations';
+        copy_dir(
+            __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'database' . DIRECTORY_SEPARATOR . 'migrations',
+            $migrationsPath
+        );
         $output->writeln('Copy database margrations end');
     }
 
     public function createStatic($output)
     {
-        $staticPath = app()->getAppPath().'..'.DIRECTORY_SEPARATOR.'public'.
-            DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'layui-admin'.DIRECTORY_SEPARATOR;
-        copy_dir(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'static', $staticPath);
+        $staticPath = app()->getAppPath() . '..' . DIRECTORY_SEPARATOR . 'public' .
+            DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'layui-admin' . DIRECTORY_SEPARATOR;
+        copy_dir(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'static', $staticPath);
         $output->writeln('Copy resources end');
     }
 }

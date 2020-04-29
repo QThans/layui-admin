46d4452d1cdc8ad52b38529e06b7f92ee0cc3da6
gamedevil <gamedevil520@gmail.com>
2020-04-29 15:34:08 +0800

    修复多文件、多图片上传只提交单文件的问题

--- src/form/Upload.php
+++ src/form/Upload.php
@@ -11,7 +11,6 @@
 namespace thans\layuiAdmin\form;
 
 use thans\layuiAdmin\Traits\Field;
-use think\facade\Filesystem;
 
 class Upload
 {
@@ -38,19 +37,25 @@ class Upload
     public $multiple = false;
     //设置同时可上传的文件数量
     public $number = 1;
+    //多文件参数
+    public $many = '';
 
+    public function many($array)
+    {
+        if($this->multiple) $this->many = '[]';
+
+        return $this;
+    }
     public function end()
     {
         $this->obj->module('upload');
-        $code = $this->obj->builder->display(__DIR__ . DIRECTORY_SEPARATOR . 'stub'
-            . DIRECTORY_SEPARATOR . 'upload.js.stub', $vars = [
+        $code = $this->obj->builder->display(__DIR__.DIRECTORY_SEPARATOR.'stub'
+            .DIRECTORY_SEPARATOR.'upload.js.stub', $vars = [
             'self' => $this,
         ]);
-        $this->obj->script('upload_js_' . $this->id, $code);
-        $this->obj->submitStartSctipt(
-            'upload_start_script_' . $this->id,
-            'delete data.field.' . $this->field . ';'
-        );
+        $this->obj->script('upload_js_'.$this->id, $code);
+        $this->obj->submitStartSctipt('upload_start_script_'.$this->id,
+            'delete data.field.'.$this->field.';');
         $val    = isset($this->obj->data[$this->name])
             ? $this->obj->data[$this->name] : '';
         $script = '';
@@ -59,38 +64,28 @@ class Upload
                 $script .= $this->appendInput($k, $v);
                 $script .= $this->appendView($k, $v);
             }
-        } elseif ($this->value) {
-            $script = $this->appendInput(0, $this->value);
-            $script .= $this->appendView(0, $this->value);
-        } else {
-            $script = $this->appendInput("'+number_{$this->id}+'", "'+v+'");
-            $script .= $this->appendView("'+number_{$this->id}+'", "'+v+'");
-            $script = <<<EOD
-            $.each(data_{$this->obj->id},function(k,v){
-                if(k == '{$this->name}'){
-                    {$script}
-                    return false;
-                }
-            });
-EOD;
+        } elseif ($val) {
+            $script = $this->appendInput(0, $val);
+            $script .= $this->appendView(0, $val);
         }
-        $this->obj->setValueScript('upload_' . $this->id, $script);
+
+        $this->obj->setValueScript('upload_'.$this->id, $script);
     }
 
     protected function appendInput($k, $val)
     {
+        if($this->multiple) $this->many = '[]';
         return <<<EOD
-$('#{$this->id}_upload_list').append('<input name="{$this->name}" type="hidden" class="value_{$this->id}_{$k}" value="{$val}">');
+$('#{$this->id}_upload_list').append('<input name="{$this->name}{$this->many}" type="hidden" class="{$this->multiple} value_{$this->id}_{$k}" value="{$val}">');
 number_{$this->id}++;
 EOD;
     }
 
     protected function appendView($k, $v)
     {
-        $v = Filesystem::getUrl($v);
         if ($this->uploadType == 'image') {
             return <<<EOD
-                $('#{$this->id}_upload_list').append('<div id="dim_{$this->id}_$k" class="dimback"><img id="file_{$this->id}_$k" src="$v" alt="" class="layui-upload-img view_{$this->id}" ><span class="status_$k">已上传</span><i data-id="$k" class="layui-icon-close layui-icon removeimg_{$this->id}"></i></div>');
+                $('#{$this->id}_upload_list').append('<div id="dim_{$this->id}_$k" class="dimback"><img id="file_{$this->id}_$k" src="$v" alt="" class="layui-upload-img  view_{$this->id}" ><span class="status_$k">已上传</span><i data-id="$k" class="layui-icon-close layui-icon removeimg_{$this->id}"></i></div>');
 EOD;
         } else {
             return <<<EOD
